d7420610436bc4f4ae80d93d5ff1a3a16c83b53f
Jiawen Zhou <jiz548@ucsd.edu>
2020-06-14 13:55:39 -0700

    iris added

--- Iris/iris.py
+++ Iris/iris.py
@@ -1,3 +1,9 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+data = pd.read_table(r'iris.data',sep=',',header=None)
+iris_targets = data[4]
+iris_features = data.iloc[:,0:4]
 
